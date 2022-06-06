5522ee3136fb1d9caa4f72c0e5ffa898deee2135
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-06 12:29:50 +0100

    still formatting

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <Nav />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Nav from "~/components/App/Nav.vue"
+import Footer from "~/components/App/Footer.vue"
+export default {
+  components: {
+    Nav,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mayworks',
+    title: 'Mayworks',
     htmlAttrs: {
       lang: 'en',
     },
@@ -18,7 +18,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/main.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
