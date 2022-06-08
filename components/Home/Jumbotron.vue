924a1400e83df27956c124a5ddbfeb34d8459643
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-08 09:24:01 +0100

    made some changes

--- components/Home/Jumbotron.vue
+++ components/Home/Jumbotron.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="jumbotron">
-
+    <h1>We are committed to <br> helping you in every way.</h1>
+    <p>We execute contracts, develop websites, grow brand names, export goods and so much more. Just reach out, we can find something for you.</p>
+    <NuxtLink to="/quote">Get Quote</NuxtLink>
   </div>
 </template>
 
@@ -16,10 +18,42 @@ export default {
   background-image: url("https://images.unsplash.com/photo-1597334948330-38795f25d05d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1700&q=80");
   background-attachment: fixed;
   background-repeat: no-repeat;
+  background-color: $dark;
   object-fit: fill;
-  height: 1200px;
+  height: 700px;
   max-width: 100%;
   color: $light;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+
+  h1 {
+    font-size: 36px;
+    padding: 1rem 0;
+  }
+
+  p { 
+    width: 380px;
+    line-height: 1.875;
+    font-size: 18px;
+    padding: 1rem 0;
+  }
+
+  a {
+    background: $light;
+    color: $primary;
+    padding: 12px 16px;
+    text-decoration: none;
+    border-radius: 8px;
+
+    &:hover {
+      background: $primary;
+      color: $light;
+
+    }
+  }
 
   @media screen and (max-width: $medium) {
     max-height: 800px;
