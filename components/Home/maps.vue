c45b5cc58aeadad3e7ae7b7dcb470d24d7b92bc1
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-11 11:39:03 +0100

    edited details of maps

--- components/Home/maps.vue
+++ components/Home/maps.vue
@@ -4,32 +4,32 @@
     <div class="content">
       <div class="card">
         <div class="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.142733117981!2d3.349837015003017!3d6.503609225197974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8d8b74245bed%3A0x98cda859077b56f4!2sTrack%20IT!5e0!3m2!1sen!2sng!4v1654336974311!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.5608281938075!2d3.3885197150029303!3d6.4503838257979895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b1073ccdd79%3A0xb77c5f8bd50972a0!2s1%20Joseph%20Street%2C%20Lagos%20Island%20102273%2C%20Lagos!5e0!3m2!1sen!2sng!4v1654943829318!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div class="info">
           <h4>Address</h4>
-          <p>40, Oyekan Street Off Ogunlana Drive Surulere, Lagos.</p>
+          <p>1 Joseph Street, Lagos Island 102273</p>
           <h4>Hours</h4>
           <p>Monday - Friday</p>
           <p>8am - 5pm</p>
           <h4>Contacts</h4>
-          <p><a href="tel:+234 901 563 0453">+234 901 563 0453</a></p>
-          <p><a href="mailto:info@trackit.com.ng">info@trackit.com.ng</a></p>
+          <p><a href="tel:+234 808 898 5577">+234 808 898 5577</a></p>
+          <p><a href="mailto:kingsleyn24@yahoo.com">kingsleyn24@yahoo.com</a></p>
         </div>
       </div>
       <div class="card">
         <div class="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15759.903889839848!2d7.455027980789247!3d9.06595313563224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0b146aaaaaab%3A0x1f731775230620f7!2sTrack%20IT%20Limited!5e0!3m2!1sen!2sng!4v1654354352329!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.2621849623997!2d3.330804115002993!3d6.488446825369379!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8eaeb967741f%3A0x9f2b68448eafa77b!2s79%20Sanya%20St%2C%20Ijesha%20Tedo%20101241%2C%20Lagos!5e0!3m2!1sen!2sng!4v1654943894270!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div class="info">
           <h4>Address</h4>
-          <p>Suite 11b, DBM plaza Wuse Zone1, Abuja.</p>
+          <p>79 Sanya St, Surulere 101241.</p>
           <h4>Hours</h4>
           <p>Monday - Friday</p>
           <p>8am - 5pm</p>
           <h4>Contacts</h4>
-          <p><a href="tel:+234 802 317 8729">+234 802 317 8729</a></p>
-          <p><a href="mailto:info@trackit.com.ng">info@trackit.com.ng</a></p>
+          <p><a href="tel:+234 806 233 8622">+234 806 233 8622</a></p>
+          <p><a href="mailto:tecpastryworld@gmail.com">tecpastryworld@gmail.com</a></p>
         </div>
       </div>
     </div>
