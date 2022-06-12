fc5e9305c32caf7869375e08c877367a1329f641
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-06-12 06:42:50 +0100

    made changes to map

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,13 +1,15 @@
 <template>
-  
+  <div class="about">
+    <form>
+      <label for="email">Email</label>
+      <input type="text" name="email" placeholder="We'll reply to you here">
+    </form>
+    <div class="map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.5608281938075!2d3.3885197150029303!3d6.4503838257979895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b1073ccdd79%3A0xb77c5f8bd50972a0!2s1%20Joseph%20Street%2C%20Lagos%20Island%20102273%2C%20Lagos!5e0!3m2!1sen!2sng!4v1654943829318!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
-<style>
+<style lang="scss" scoped>
 
 </style>
